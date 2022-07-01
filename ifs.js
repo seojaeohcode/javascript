798ba5a83c1503ae937a99ead0895a607dd09c19
seojaeoh <seojaeohcode@gmail.com>
2022-07-01 13:02:26 +0900

    js

---
+++ ifs.js
@@ -0,0 +1,30 @@
+let condition = true;
+condition = 30 > 6;
+
+number_first = 30;
+number_second = 6;
+
+condition = number_first > number_second;
+let condition_second = (number_first <= number_second) 
+
+condition = condition && condition_second;
+
+if (condition) {
+    console.log("in if state!");
+}
+
+if (!condition) {
+    console.log("not in if state!");
+}
+
+let score = 50;
+
+if (score >= 90) {
+    console.log("Well !");
+} else if (score >= 50) {
+    console.log("good !");
+} else {
+    console.log("not bad !");
+}
+
+console.clear();
---
+++ variableBoolean.js
@@ -0,0 +1,13 @@
+let flag = true; // 1,34242
+flag = false;
+
+let condition = 20 > 5;
+condition = 20 <= 5;
+
+let condition_first = 15 > 5;
+let condition_second = 15 < 7;
+
+let result = condition_first || condition_second;
+result = condition_first && condition_second;
+!result;
+console.clear();
